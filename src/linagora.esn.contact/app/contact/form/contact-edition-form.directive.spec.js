c3dddd5abc3e62d4557504005c62748fa9b6c412
Khaled FERJANI <kferjani@linagora.com>
2021-04-05 19:13:22 +0100

    #101 display the shared addressbook owner in the selection when creating a new contact

--- src/linagora.esn.contact/app/contact/form/contact-edition-form.directive.spec.js
+++ src/linagora.esn.contact/app/contact/form/contact-edition-form.directive.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* global chai: false */
+/* global chai, sinon: false */
 
 var expect = chai.expect;
 
@@ -8,8 +8,18 @@ describe('The contactEditionForm directive', function() {
   var $compile, $rootScope, $scope, session;
   var CONTACT_AVATAR_SIZE, DEFAULT_ADDRESSBOOK_NAME, CONTACT_COLLECTED_ADDRESSBOOK_NAME;
   var contactAddressbookService, esnConfigMock;
+  let userAPIMock, contactAddressbookDisplayServiceMock;
 
   beforeEach(function() {
+    contactAddressbookDisplayServiceMock = {
+      convertShellsToDisplayShells: sinon.spy(function(shells) {
+        return shells.map(shell => ({
+          bookName: shell.bookName,
+          shell: shell
+        }));
+      }),
+      sortAddressbookDisplayShells: shells => shells
+    };
     angular.mock.module('esn.core');
     angular.mock.module('linagora.esn.contact');
     angular.mock.module(function($provide) {
@@ -17,7 +27,24 @@ describe('The contactEditionForm directive', function() {
         return $q.when(true);
       };
 
+      const user = {
+        data: {
+          id: 123456,
+          firstname: 'john',
+          lastname: 'doe'
+        }
+      };
+
+      userAPIMock = {
+        user: sinon.mock().returns(
+          $q.when(user)
+        ),
+        currentUser: () => $q.when(user)
+      };
+
       $provide.value('esnConfig', esnConfigMock);
+      $provide.value('userAPI', userAPIMock);
+      $provide.value('contactAddressbookDisplayService', contactAddressbookDisplayServiceMock);
     });
   });
 
@@ -46,18 +73,31 @@ describe('The contactEditionForm directive', function() {
       urls: []
     };
     contactAddressbookService.listAddressbooksUserCanCreateContact = function() {
-      return $q.when([{
-        bookName: DEFAULT_ADDRESSBOOK_NAME,
-        href: '/addressbooks/userId/' + DEFAULT_ADDRESSBOOK_NAME + '.json'
-      }, {
-        bookName: CONTACT_COLLECTED_ADDRESSBOOK_NAME,
-        href: '/addressbooks/userId/' + CONTACT_COLLECTED_ADDRESSBOOK_NAME + '.json'
-      }]);
+      return $q.when([
+        {
+          bookName: DEFAULT_ADDRESSBOOK_NAME,
+          href: '/addressbooks/userId/' + DEFAULT_ADDRESSBOOK_NAME + '.json'
+        },
+        {
+          bookName: CONTACT_COLLECTED_ADDRESSBOOK_NAME,
+          href: '/addressbooks/userId/' + CONTACT_COLLECTED_ADDRESSBOOK_NAME + '.json'
+        },
+        {
+          bookName: 'shared',
+          href: '/addressbooks/123456/123456.json',
+          group: undefined,
+          isSubscription: true,
+          source: {
+            bookId: 123456
+          }
+        }
+      ]);
     };
   }));
 
   beforeEach(function() {
     session.user = { _id: 'userId' };
+    session.fetchUser = () => $q.when(session.user);
   });
 
   function initDirective(scope) {
@@ -72,5 +112,11 @@ describe('The contactEditionForm directive', function() {
     var element = initDirective($scope);
 
     expect(element.isolateScope().avatarSize).to.equal(CONTACT_AVATAR_SIZE.bigger);
+  });
+
+  it('should try to load the user using the userId collected from the shared addressbook', () => {
+    initDirective($scope);
+
+    expect(userAPIMock.user).to.have.been.calledWith(123456);
   });
 });
